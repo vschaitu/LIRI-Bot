8c6399de48dd57080093e30dedfb52723ec8fd51
vschaitu <vschaitu@gmail.com>
2018-05-19 20:08:14 -0500

    Added moment npm and fs

--- liri.js
+++ liri.js
@@ -1,4 +1,6 @@
 // import { twitter, spotify } from "./keys";
+// add fs pkg required for wirting logs
+var fs = require("fs");
 
 // add https npm request & object instance
 var request = require('request');
@@ -17,9 +19,44 @@ var client = new Twitter(keys.twitter);
 var Spotify = require('node-spotify-api');
 var spotify = new Spotify(keys.spotify);
 
+// add moment npm to get timestamp for logging
+var moment = require('moment');
+
 // Get input request type from CLI argument
 var ipReqType = process.argv[2];
 
 // Get input Additonal input from CLI arguments
 var ipReqData = process.argv.slice(3).join(" ");
 
+// log the input parameters
+writelog('Request Type: ' + ipReqType + ' Request Addtn Data: ' + ipReqData);
+
+switch (ipReqType) {
+    case 'my-tweets':
+        getTweets();
+        break;
+    case 'spotify-this-song':
+        break
+    case 'movie-this':
+        break;
+    case 'do-what-it-says':
+        break;
+    default:
+        console.log(moment().format());
+        console.log('********Invalid request!!!*********');
+}
+
+
+function writelog(data) {
+    var logrec = '\n' + moment().format() + " " + data;
+    fs.appendFile("log.txt", logrec, function (err) {
+        if (err) throw err;
+    });
+
+}
+
+function getTweets() {
+    client.get('search/tweets', { q: 'node.js' }, function (error, tweets, response) {
+        console.log(tweets);
+    });
+}
